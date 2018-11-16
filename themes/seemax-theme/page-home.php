99d183096886f4e4ee22ded0b89865556a4b57d8
Max Mearsheimer <max@seemaxwork.com>
2018-11-16 16:44:20 -0600

    Initial commit

---
+++ themes/seemax-theme/page-home.php
@@ -0,0 +1,11 @@
+<?php /* Template Name: Home */ get_header(); ?>
+<main class="home-page">
+	<?php while (have_posts()) : the_post(); ?>
+		<section class="main-section">
+			<div class="content">
+				<?php the_content(); ?>
+			</div>
+		</section>
+	<?php endwhile; ?>
+</main>
+<?php get_footer(); ?>
