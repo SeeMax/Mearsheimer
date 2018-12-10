58af1ccfcd5bb436593b4f493cc5fdbbf8cb4141
Max Mearsheimer <max@seemaxwork.com>
2018-12-10 15:17:19 -0600

    Tweaks

--- themes/seemax-theme/page-teaching.php
+++ themes/seemax-theme/page-teaching.php
@@ -7,19 +7,26 @@
 				<?php the_content(); ?>
 			</div>
 		</section>
-		<section class="teaching-links-section">
+		<section class="courses-section">
 			<div class="content">
+				<h2><?php the_field('courses_title');?></h2>
+				<p class="course-section-description">
+					<?php the_field('courses_description');?>
+				</p>
+				<?php the_field('courses_note');?>
 				<?php if(have_rows('course_links')): while (have_rows('course_links')): the_row();?>
-					<div class="single-course-link">
-						<div class="single-course-name">
-							<?php the_sub_field('single_course_name'); ?>
-						</div>
-						<div class="single-course-date">
-							<?php the_sub_field('single_course_date'); ?>
+					<div class="single-course">
+						<div class="single-course-info">
+							<h3 class="single-course-name">
+								<?php the_sub_field('single_course_name'); ?>
+							</h3>
+							<div class="single-course-date">
+								<?php the_sub_field('single_course_date'); ?>
+							</div>
 						</div>
 						<div class="button">
 							<a class="c-block-fill" href='<?php the_sub_field('single_course_pdf'); ?>'></a>
-							Download PDF
+							<i class="far fa-arrow-alt-circle-down"></i>
 						</div>
 					</div>
 				<?php endwhile; endif;?>
